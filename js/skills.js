7df21e1f6869c7817c5adfb3ebd523e748181ab7
HelmetBug <Powpengo@gmail.com>
2021-06-10 20:28:43 -0400

    added skills in skill js

--- js/skills.js
+++ js/skills.js
@@ -21,8 +21,35 @@ function checkSkills(level){
 
 function initSkills(){
 	h.globalSkills = [
-  	    createSkill("Peck","A basic melee attack using a beak", 2, console.log("Peck")),
-	    createSkill("Float","A basic defensive move halving enemy damage", 5, console.log("Float")),
-	    createSkill("Duster","A special magic attack that deals damage and lowers enemy accuracy", 7, console.log("Duster")),
+  	    createSkill("Peck","A basic melee attack using a beak", 2, Peck()),
+	    createSkill("Float","A basic defensive move halving enemy damage", 5, Float()),
+	    createSkill("Duster","A special magic attack that deals damage and lowers enemy accuracy", 7, Duster()),
 	];
+}
+
+function Peck() {
+	chance = h.randomInt(0,10);
+	
+	if (chance > 4){
+		console.log("peck");	
+	}
+	
+}
+
+function Float() {
+	chance = h.randomInt(0,10);
+	
+	if (chance > 4){
+		console.log("float");	
+	}
+	
+}
+
+function Duster() {
+	chance = h.randomInt(0,10);
+	
+	if (chance > 4){
+		console.log("dust");	
+	}
+	
 }
