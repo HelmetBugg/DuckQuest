f1ed7f7937a8fdde12bdcf348503f8c023622e04
HelmetBug <Powpengo@gmail.com>
2021-09-23 20:35:09 -0400

    whoops don't check the new file size

---
+++ res/maps/Chars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Chars" tilewidth="16" tileheight="16" tilecount="120" columns="8">
+ <image source="../images/Player0.png" width="128" height="240"/>
+</tileset>
