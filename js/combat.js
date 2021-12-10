74829e03b2015dcea6cae0cd1f9c1408a05d0aca
IJMadigan <madigaij195@gmail.com>
2021-12-09 20:49:18 -0500

    Added skills menu.
    Added skills menu to push it to making a card battle system.

--- js/combat.js
+++ js/combat.js
@@ -47,27 +47,30 @@ function getAttacked() {
 
 function spawnCombatMenu(){
 	var menu = h.rectangle(500, 250, color.background);
+
+
+
 	/*menu.runButton = button(0, 200, "Run");
 	menu.attackButton = button(100, 200, "Attack");
 	menu.skillsButton = button(255, 200, "Skills");
 	menu.skillsButton.interact = false;
 	menu.skillsButton.alpha = 0.5;
 	*/
-	//menu.skillsMenu= skillsMenu();
+	menu.skillsMenu= skillsMenu();
 	
 	menu.enemyName = h.text("Enemy Name:\n");
 	menu.enemyName.style = fontStyle;
-    menu.enemyName.y = 20;
+    menu.enemyName.y = 10;
     menu.enemyName.x = 270;
 
     menu.enemyHealth = h.text("Enemy Health:\n " + 0/0);
 	menu.enemyHealth.style = fontStyle;
-    menu.enemyHealth.y = 50;
+    menu.enemyHealth.y = 30;
     menu.enemyHealth.x = 270;
 
     menu.playerHealth = h.text("Player Health:\n " + h.player.stat.get("current_health") + " / " + h.player.stat.get("max_health"));
 	menu.playerHealth.style = fontStyle;
-	menu.playerHealth.y = 50;
+	menu.playerHealth.y = 30;
 
 	var combatLog = new createDialogBox();
 	menu.addChild(menu.enemyName, menu.enemyHealth, menu.playerHealth, combatLog);
@@ -105,4 +108,23 @@ function cleanupCombat(combatMenu){
 	cleanup(combatMenu);
 	h.inCombat = false;
 	checkQuests();
+}
+
+function skillsMenu(){
+	
+	var skills = checkSkills(h.player.stat.get("level"));
+	var menu = h.rectangle(1, 1, color.background);
+	menu.x = -50;
+
+	for (i=0; i <  skills.length; i++) {
+		var desc = skills[i].name + " DMG:"+ skills[i].damage + " CHANCE:" + skills[i].accuracy;
+		menu.addChild(button(50,175+50*i, desc));
+		
+
+		
+		
+	}
+
+
+
 }
