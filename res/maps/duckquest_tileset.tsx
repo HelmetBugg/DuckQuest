62d6e724f371e47d0d117be09201541d637ed8b8
HelmetBug <Powpengo@gmail.com>
2022-04-28 19:42:18 -0400

    Finished Tent map, and collisions for it

--- res/maps/duckquest_tileset.tsx
+++ res/maps/duckquest_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="duckquest_tileset" tilewidth="16" tileheight="16" tilecount="1656" columns="46">
- <image source="../images/duckquest_tileset.png" trans="ffffff" width="744" height="576"/>
+<tileset version="1.8" tiledversion="1.8.4" name="duckquest_tileset" tilewidth="16" tileheight="16" tilecount="1656" columns="46">
+ <image source="../images/duckquest_tileset.png" width="744" height="576"/>
 </tileset>
