20fcdc7acfb8fbef5499bf3b8063101992ea58cc
IJMadigan <madigaij195@gmail.com>
2022-05-19 19:30:51 -0400

    split the index into a new file
    split util.js into new file

--- js/util.js
+++ js/util.js
@@ -222,62 +222,7 @@ function teleportCollisionCheck() {
     return false;
 }
 
-function initplayer() {
-    if (h.player == null) {
-        h.player = h.sprite("res/images/duckman.png");
-    }
-    h.player.directionFacingBox = h.rectangle(16, 16, "white", "black", 0, 0, 0);
-    h.player.directionFacingBox.visible = false;
-	let stat = new Map();
-	stat.set("experience", 0);
-	stat.set("next_level", 14);
-	stat.set("level", 1);	
-	stat.set("strength", 5);
-	stat.set("max_health", 100);
-	stat.set("current_health", 50);
-	stat.set("intelligence", 5);
-	h.player.encounter_Chance = 0;
-	h.player.killed = {
-		"total": 0,
-		"slime": 0,
-		"AligatorBoss": 0,
-		"SpiderBoss": 0,
-		"DragonFlyBoss": 0,
-		"SnakeBoss": 0,
-		"ClownfishBoss": 0,
-		"GooseBoss": 0
-	};
-	h.player.status = {
-		"protected": false,
-		"rage": false
-	};
-	h.player.stat = stat;
-	h.player.skills = [];
-	h.player.quests = [];
-	h.player.tweening = false;
 
-	// Kick off default quests
-	startKillAligatorBoss();
-	startClownfishQuest();
-	startDragonflyQuest();
-
-	h.player.checkQuests = function(questName){
-		for (let i=0; i < h.player.quests.length; i++)   {
-			if (h.player.quests[i].name == questName && h.player.quests[i].active){	
-				return true;
-			}
-		}
-		return false;
-	}
-
-    h.player.henshin = function () {
-        sentai = true
-		h.player.stat.set("max_health",40000);
-		h.player.stat.set("strength",40000);
-		levelUp();
-		h.player.tint = 0xff1808;
-    }
-}
 
 function gainExperience(experience) {
     var current_experience = h.player.stat.get("experience");
