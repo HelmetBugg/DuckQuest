4cce0a791c29ce9ee20cc97ed3d97f46647f5f59
IJMadigan <madigaij195@gmail.com>
2022-05-19 20:25:09 -0400

    Added bitmaps and collisions for throneroom

---
+++ res/maps/Brandon hacky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Brandon hacky" tilewidth="16" tileheight="16" tilecount="1225" columns="35">
+ <image source="throne_room.png" trans="000000" width="560" height="560"/>
+</tileset>
