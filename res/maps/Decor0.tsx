c4a1f232015caf958b1a775ca44ac24366d3d008
malceore <malceore@gmail.com>
2022-05-19 20:07:27 -0400

    adding throne room map

--- res/maps/Decor0.tsx
+++ res/maps/Decor0.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="Decor0" tilewidth="16" tileheight="16" tilecount="176" columns="8">
- <image source="../../../../../../../Downloads/DawnLike/Objects/Decor0.png" width="128" height="352"/>
+ <image source="../../DawnLike/Objects/Decor0.png" trans="ffffff" width="128" height="352"/>
 </tileset>
