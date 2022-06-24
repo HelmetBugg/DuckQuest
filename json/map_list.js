abe6aa498d6cc84a7a3f89842cb6a879b50162c3
TheCrusty <zezoku@yahoo.com>
2022-06-23 20:34:49 -0400

    added end of game town and removed id value from maps

--- json/map_list.js
+++ json/map_list.js
@@ -1,6 +1,5 @@
 maps = [{
         "name": "Pondville",
-        "id": 0,
         "sprite": "res/maps/village_map.png",
         "collisionMap": "res/maps/village_collisions.bmp",
         "enemies": [],
@@ -106,7 +105,6 @@ maps = [{
         ]
     }, {
         "name": "Overworld",
-        "id": 0,
         "sprite": "res/maps/overworld_map.png",
         "collisionMap": "res/maps/overworld_collision.bmp",
         "enemies": [{
@@ -277,7 +275,6 @@ maps = [{
         ]
     }, {
         "name": "Alligator Hideout",
-        "id": 0,
         "sprite": "res/maps/aligator_hideout.png",
         "collisionMap": "res/maps/alligator_hideout_collisions.bmp",
         "enemies": [{
@@ -380,7 +377,6 @@ maps = [{
         ]
     }, {
         "name": "Spider Cave",
-        "id": 0,
         "sprite": "res/maps/spider_cave.png",
         "collisionMap": "res/maps/spider_cave_collisions.bmp",
         "enemies": [{
@@ -451,7 +447,6 @@ maps = [{
 
     }, {
         "name": "Alligator Hideout",
-        "id": 0,
         "sprite": "res/maps/aligator_hideout.png",
         "collisionMap": "res/maps/alligator_hideout_collisions.bmp",
         "enemies": [{
@@ -553,7 +548,6 @@ maps = [{
         ]
     }, {
         "name": "Bog",
-        "id": 0,
         "sprite": "res/maps/new_swamp_map.png",
         "collisionMap": "res/maps/new_swamp_map_collisions.bmp",
         "enemies": [{
@@ -619,7 +613,6 @@ maps = [{
         ]
     }, {
         "name": "Sky Cliffs",
-        "id": 0,
         "sprite": "res/maps/SkyCliffs.png",
         "collisionMap": "res/maps/skycliffscollisions.bmp",
         "enemies": [{
@@ -698,7 +691,6 @@ maps = [{
         ]
     }, {
         "name": "Tent",
-        "id": 0,
         "sprite": "res/maps/Tent.png",
         "collisionMap": "res/maps/TentCollisions.bmp",
         "enemies": [],
@@ -727,7 +719,6 @@ maps = [{
         ]
     }, {
         "name": "Maze",
-        "id": 0,
         "sprite": "res/maps/maze.png",
         "collisionMap": "res/maps/maze_collisions.bmp",
         "enemies": [],
@@ -779,7 +770,6 @@ maps = [{
             }
 	]}, {
             "name": "Courtyard",
-            "id": 0,
             "sprite": "res/maps/courtyard.png",
             "collisionMap": "res/maps/courtyard_collisions.bmp",
             "enemies": [],
@@ -843,7 +833,6 @@ maps = [{
                 }
 		]}, {
                 "name": "Throne Room",
-                "id": 0,
                 "sprite": "res/maps/throne_room.png",
                 "collisionMap": "res/maps/throne_room_collisions_small2.bmp",
                 "enemies": [],
@@ -879,8 +868,112 @@ maps = [{
                         "index": 2,
                         "width": 16,
                         "height": 16,
-                        "dialog": ["So, you're finally here Duckman.", "Unfortunately, for you, this battle will be no different than our last...", "Except this time I'll make sure I finish the job!", GoosemanBoss],
-                        "questTriggers": "Kill Nemo"
+                        "dialog": ["So, you're finally here Duckman.", "Unfortunately, for you, this battle will be no different than our last...", "Except this time I'll make sure I finish the job!", GoosemanBoss]
+                    }
+                ]
+            },{
+                "name": "PondvilleEndOfGame",
+                "sprite": "res/maps/village_map.png",
+                "collisionMap": "res/maps/village_collisions.bmp",
+                "enemies": [],
+                "player_spawn_x": 640,
+                "player_spawn_y": 400,
+                "tileWidth": 16,
+                "tileHeight": 16,
+                "tilesWide": 45,
+                "tilesHigh": 45,
+                "triggers": [{
+                        "id": 28,
+                        "name": "Rocky",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 528,
+                        "y": 320,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["Duckman, you're alive dude!", "Wh-what has Gooseman done to you?", "He's clipped your wings and cut your feet webs!?", "I see.. you won't be able to swim or fly again.", "But at least you came back with your life."],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 29,
+                        "name": "James",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 608,
+                        "y": 160,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["..."],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 24,
+                        "name": "Jim",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 624,
+                        "y": 448,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["It's unbelievable what Gooseman's done..", "He's left so many from our village dead in his quest to take over the pond.", "So many that we've started burying people outside the walls.", "My Brother James is out there now.", "Please stop him Duckman, not for me, but.. for them."],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 70,
+                        "name": "Mayor D.",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 336,
+                        "y": 272,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["I am very concerned...", "The Goose Gang...", "has taken over...", "the Pond...", "Please.. ", "Save us Duckman!"],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 2,
+                        "name": "Croaky",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 528,
+                        "y": 464,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["Fa-lalala~", "Tra-lalala", "OH, did you enjoy my song?", "Kill three slimes for me and I'll sing you something new!", startKillThreeSlimesQuest],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 11,
+                        "name": "IsoKid",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 544,
+                        "y": 128,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["I've been searching for monster friends in the grass all day!", "I hope I can find a \nshiny one!"],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 35,
+                        "name": "Rattavan",
+                        "type": "npc",
+                        "visible": true,
+                        "x": 336,
+                        "y": 448,
+                        "width": 16,
+                        "height": 16,
+                        "dialog": ["Welcome to Rattavan's Gently \nUsed Fine Goods!", "No Refunds kid.", "Hey kid, buisiness has been slow.  Why don't you go uh... clean out the cave to the east.", "I might be able to sell the local yuppies some spelunking tours.", "How about it?", "...", "You dont talk much do ya?", startClearCavesForRat],
+                        "spriteSheet": "res/images/Player0.png"
+                    }, {
+                        "id": 6,
+                        "name": "teleporterOverworld",
+                        "rotation": 0,
+                        "type": "teleporterTile",
+                        "visible": true,
+                        "x": 672,
+                        "y": 336,
+                        "width": 16,
+                        "height": 16,
+                        "destination": "Overworld",
+                        "spawn": {
+                            "x": 496,
+                            "y": 304
+                        }
                     }
                 ]
             }
