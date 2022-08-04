3900b64efd2e24d998aa351f9b9543d841a2617b
malceore <malceore@gmail.com>
2022-08-04 19:28:46 -0400

    adding music handler class to handle music playback.

---
+++ js/musicHandler.js
@@ -0,0 +1,19 @@
+class MusicHandler {
+	constructor(){
+		this.music = h.sound("res/sounds/damage.wav");
+		this.fadeTime = 2;
+		this.music.loop = true;
+	}
+
+	setSong(song){
+		this.music.fadeOut(this.fadeTime);
+		this.music = h.sound(song);
+		this.music.loop = true;
+		this.play();
+	}
+
+	play(){
+		this.music.play();
+	}
+
+};
